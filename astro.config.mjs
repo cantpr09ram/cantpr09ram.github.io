66c2f3afe3a118011bc4b67fd5229b4e29f21486
cantpr09ram <bochainwu@gmail.com>
2025-10-12 17:16:41 +0800

    fix: Update site URL in Astro config and streamline deployment workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://example.com",
+	site: "https://cantpr09ram.github.io",
 	integrations: [mdx(), sitemap()],
 	vite: {
 		plugins: [tailwindcss()],
